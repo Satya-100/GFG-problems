82f1dda1d7846982749aaf70fb91e50fe569ee67
Satya Prakash Mahour <99254801+Satya-100@users.noreply.github.com>
2025-08-03 14:04:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sums/subset-sums.cpp
+++ Difficulty: Medium/Subset Sums/subset-sums.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    void helper(int i, int n, int sum, vector<int>& res, vector<int> temp, vector<int>& arr){
+    void helper(int i, int n, int sum, vector<int>& res, vector<int>& arr){
         if(i>=n){
             res.push_back(sum);
             return ;
         }
         
-        temp.push_back(arr[i]);
-        helper(i+1, n, sum+arr[i], res, temp, arr);
-        temp.pop_back();
-        helper(i+1, n, sum, res, temp, arr);
+        helper(i+1, n, sum+arr[i], res, arr);
+        helper(i+1, n, sum, res, arr);
     }
     
   public:
     vector<int> subsetSums(vector<int>& arr) {
         // code here
         vector<int> res;
-        helper(0, arr.size(), 0, res, {}, arr);
+        helper(0, arr.size(), 0, res, arr);
         return res;
     }
 };
