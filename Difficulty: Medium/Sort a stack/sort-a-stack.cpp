fe9250ad89486c4116a67480a724ee2140a1768b
Satya Prakash Mahour <99254801+Satya-100@users.noreply.github.com>
2025-08-17 13:46:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -0,0 +1,33 @@
+/*The structure of the class is
+class SortedStack{
+public:
+    stack<int> s;
+    void sort();
+};
+*/
+
+/* The below method sorts the stack s
+you are required to complete the below method */
+void insert(stack<int>& s, int temp){
+    if(s.empty() || s.top()<=temp){
+        s.push(temp);
+        return ;
+    }
+    
+    int top = s.top();
+    s.pop();
+    insert(s, temp);
+    s.push(top);
+}
+
+void SortedStack ::sort() {
+    // Your code here
+    if(s.size()==1){
+        return ;
+    }
+    
+    int temp = s.top();
+    s.pop();
+    sort();
+    insert(s, temp);
+}
